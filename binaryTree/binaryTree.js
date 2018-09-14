13e95ed8b344fa599d7c5b4090d8ec385e12ab18
Jay D. McHugh <jaydmchugh@gmail.com>
2018-09-14 14:39:57 -0500

    Adding binary tree skeleton

---
+++ binaryTree/binaryTree.js
@@ -0,0 +1,32 @@
+// This is the implementation of a binary tree
+// Best used to allow the sorting of data to be
+// split out across receiving the data and
+// storing it. This is best when the data is
+// not all known at once.
+
+function BinaryTree() {
+	this._root = null;
+}
+
+BinaryTree.prototype = {
+	constructor: BinaryTree,
+
+	add: function(value) {
+	},
+
+	contains: function(value) {
+	},
+
+	remove: function(value) {
+	},
+
+	size: function() {
+	},
+
+	toArray: function() {
+	},
+
+	toString: function() {
+	}
+};
+
